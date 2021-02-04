7f1074a252dabe820635add0a4b72d44a3216deb
jarzombeg <jarzombeg@gmail.com>
2021-02-04 18:10:37 +0000

    Initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args) {
+            //write your code here
+            sum(2, 1);
+
+        }
+
+        public static void sum(int a, int b) {
+            int c = a + b;
+            System.out.print(c);
+        }
+    }
+
